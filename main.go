dc02a5efdac8df76585ec0b519f18d0df7aacea3
ppostman <78047183+ppostman@users.noreply.github.com>
2025-02-18 17:59:53 -0600

    chore: remove unused imports

--- main.go
+++ main.go
@@ -1,27 +1,20 @@
 package main
 
 import (
-	"crypto/rand"
-	"crypto/rsa"
 	"crypto/sha256"
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
-	"io"
 	"log"
-	"math/big"
 	"net/http"
 	"net/url"
 	"os"
 	"strings"
-	"sync"
 	"time"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt"
 	"github.com/google/uuid"
-	"github.com/golang-jwt/jwt/request"
 	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
