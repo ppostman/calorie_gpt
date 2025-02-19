6f005c99b7b01b4096fcb78e3324fbb2e37cf405
ppostman <78047183+ppostman@users.noreply.github.com>
2025-02-19 15:18:55 -0600

    fix: use application URL as audience for JWT tokens

--- static/auth0-config.js
+++ static/auth0-config.js
@@ -2,7 +2,7 @@ const config = {
     clientId: 'xiSSGNYciPG0CAyHQYf4nAbA0D7OS1T5',
     redirectUri: window.location.origin + '/oauth2/callback',
     scope: 'openid profile email offline_access',
-    audience: 'https://dev-lk0vcub54idn0l5c.us.auth0.com/api/v2/',
+    audience: 'https://calorie-gpt.onrender.com',
     responseType: 'token id_token',
     tokenType: 'JWT',
 };
