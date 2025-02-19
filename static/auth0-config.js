c47df7d605a5ccc1cf6b61209e2553f9cdd0bac7
ppostman <78047183+ppostman@users.noreply.github.com>
2025-02-19 15:22:16 -0600

    fix: update audience to calorie-gpt-api

--- static/auth0-config.js
+++ static/auth0-config.js
@@ -2,7 +2,7 @@ const config = {
     clientId: 'xiSSGNYciPG0CAyHQYf4nAbA0D7OS1T5',
     redirectUri: window.location.origin + '/oauth2/callback',
     scope: 'openid profile email offline_access',
-    audience: 'https://calorie-gpt.onrender.com',
+    audience: 'https://calorie-gpt-api',
     responseType: 'token id_token',
     tokenType: 'JWT',
 };
